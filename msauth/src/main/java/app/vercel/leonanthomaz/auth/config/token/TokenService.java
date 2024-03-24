6886bcd6a94dac517b5f7d9769ae8d4233da9d17
leonanthomaz <leonan.thomaz@gmail.com>
2024-03-23 23:16:55 -0300

    refatorando o projeto

--- msauth/src/main/java/app/vercel/leonanthomaz/auth/config/token/TokenService.java
+++ msauth/src/main/java/app/vercel/leonanthomaz/auth/config/token/TokenService.java
@@ -43,6 +43,6 @@ public class TokenService {
     }
 
     private Instant genExpirationDate(){
-        return LocalDateTime.now().plusHours(3).toInstant(ZoneOffset.of("-03:00"));
+        return ;
     }
 }
